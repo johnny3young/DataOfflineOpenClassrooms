c8b47182b50e760e22fa7c8a05ec5f1e876403cd
Johnny3young <youngerblack@gmail.com>
2019-11-01 14:23:38 -0500

    adding code to the radio button

--- app/src/main/java/com/black3/app/dataofflineopenclassrooms/tripbook/TripBookActivity.kt
+++ app/src/main/java/com/black3/app/dataofflineopenclassrooms/tripbook/TripBookActivity.kt
@@ -12,26 +12,29 @@ import com.black3.app.dataofflineopenclassrooms.utils.StorageUtils
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
 
-class TripBookActivity : AppCompatActivity() {
+// 1 – PERMISSIONS MANAGEMENT
+private const val RC_STORAGE_WRITE_PERMS = 100
 
+class TripBookActivity : AppCompatActivity() {
     // 1 - FILE MANAGEMENT
     private val FILENAME = "tripBook.txt"
+
     private val FOLDERNAME = "bookTrip"
-
     //FOR DESIGN
-    private lateinit var linearLayoutExternal : LinearLayout
-    private lateinit var linearLayoutInternal : LinearLayout
+    private lateinit var linearLayoutExternal: LinearLayout
 
-    private lateinit var radioButtonExternal : RadioButton
-    private lateinit var radioButtonInternal : RadioButton
+    private lateinit var linearLayoutInternal: LinearLayout
+    private lateinit var radioButtonExternal: RadioButton
 
-    private lateinit var radioButtonExternalPublic : RadioButton
-    private lateinit var radioButtonExternalPrivate : RadioButton
+    private lateinit var radioButtonInternal: RadioButton
+    private lateinit var radioButtonExternalPublic: RadioButton
 
-    private lateinit var radioButtonInternalVolatile : RadioButton
-    private lateinit var radioButtonInternalNormal : RadioButton
+    private lateinit var radioButtonExternalPrivate: RadioButton
+    private lateinit var radioButtonInternalVolatile: RadioButton
 
-    private lateinit var editText : EditText
+    private lateinit var radioButtonInternalNormal: RadioButton
+
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +45,7 @@ class TripBookActivity : AppCompatActivity() {
         ab!!.setDisplayHomeAsUpEnabled(true)
 
         linearLayoutExternal = findViewById(R.id.trip_book_activity_external_choice)
-        linearLayoutInternal = findViewById(R.id.trip_book_activity_internal_choice)
+        linearLayoutInternal = findViewById(R.id.<)
 
         radioButtonExternal = findViewById(R.id.trip_book_activity_radio_external)
         radioButtonInternal = findViewById(R.id.trip_book_activity_radio_internal)
@@ -60,28 +63,26 @@ class TripBookActivity : AppCompatActivity() {
         }
 
         radioButtonExternal.setOnCheckedChangeListener { buttonView, isChecked ->
-            onClickRadioButton(buttonView,isChecked)
+            onClickRadioButton(buttonView, isChecked)
         }
 
         radioButtonInternalVolatile.setOnCheckedChangeListener { buttonView, isChecked ->
-            onClickRadioButton(buttonView,isChecked)
+            onClickRadioButton(buttonView, isChecked)
         }
 
         radioButtonInternalNormal.setOnCheckedChangeListener { buttonView, isChecked ->
-            onClickRadioButton(buttonView,isChecked)
+            onClickRadioButton(buttonView, isChecked)
         }
 
         radioButtonExternalPublic.setOnCheckedChangeListener { buttonView, isChecked ->
-            onClickRadioButton(buttonView,isChecked)
+            onClickRadioButton(buttonView, isChecked)
         }
 
         radioButtonExternalPrivate.setOnCheckedChangeListener { buttonView, isChecked ->
-            onClickRadioButton(buttonView,isChecked)
+            onClickRadioButton(buttonView, isChecked)
         }
-
         // 2 - Read from storage when starting
         this.readFromStorage()
-
     }
 
     // ----------------------------------
@@ -89,109 +90,176 @@ class TripBookActivity : AppCompatActivity() {
     // ----------------------------------
     fun onClickRadioButton(button: CompoundButton, isChecked: Boolean) {
 
-    if (isChecked) {
+        if (isChecked) {
 
         }
 
-     // 3 - Read from storage after user clicks on radio buttons
-     this.readFromStorage()
+        // 3 - Read from storage after user clicks on radio buttons
+        this.readFromStorage()
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-    when (item.itemId) {
-    R.id.action_save -> {
-    // 4 – Save
-    this.save()
-    return true
-
-    }
-
+        when (item.itemId) {
+            R.id.action_save -> {
+                // 4 – Save
+                this.save()
+                return true
+            }
         }
-        ...
+        //lo coloqué para saltarme el error que había acá, luego miro qué hace o qué debo poner realmente
+        return false
     }
 
     // 5 - Save after user clicked on button
     private fun save() {
 
-    if (radioButtonExternal.isChecked) {
-    writeExternalStorage() //Save on external storage
+        if (radioButtonExternal.isChecked) {
+            writeExternalStorage() //Save on external storage
 
         } else {
-    //TODO: Save on internal storage
-
+            //TODO: Save on internal storage
         }
 
     }
-    // 1 – PERMISSIONS MANAGEMENT
-    private const val RC_STORAGE_WRITE_PERMS = 100
-
-    class TripBookActivity : AppCompatActivity() {
 
     // 2 - After permission granted or refused
-     override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String>, grantResults: IntArray) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-            }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
     // ----------------------------------
     // UTILS - STORAGE
     // ----------------------------------
 
-        @AfterPermissionGranted(RC_STORAGE_WRITE_PERMS)
+    @AfterPermissionGranted(RC_STORAGE_WRITE_PERMS)
 
     // 6 - Read from storage
-
-
     private fun readFromStorage() {
+        // CHECK PERMISSION
+        if (!EasyPermissions.hasPermissions(this, WRITE_EXTERNAL_STORAGE)) {
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.title_permission),
+                RC_STORAGE_WRITE_PERMS,
+                WRITE_EXTERNAL_STORAGE
+            )
+            return
+        }
 
-            if (!EasyPermissions.hasPermissions(this, WRITE_EXTERNAL_STORAGE)) {
-                EasyPermissions.requestPermissions(this, getString(R.string.title_permission), RC_STORAGE_WRITE_PERMS, WRITE_EXTERNAL_STORAGE)
-                return
+        if (radioButtonExternal.isChecked) {
+            if (StorageUtils.isExternalStorageReadable()) {
+                // EXTERNAL
+                if (radioButtonExternalPublic.isChecked) {
+                    // External - Public
+                    editText.setText(
+                        StorageUtils.getTextFromStorage(
+
+                            Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_DOCUMENTS
+                            ), this, FILENAME, FOLDERNAME
+                        )
+                    )
+
+                } else {
+                    // External - Privatex
+                    editText.setText(
+                        StorageUtils.getTextFromStorage(
+
+                            getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!,
+                            this,
+                            FILENAME,
+                            FOLDERNAME
+                        )
+                    )
+
                 }
 
-    if (radioButtonExternal.isChecked) {
-        if (StorageUtils.isExternalStorageReadable()) {
-        // EXTERNAL
-            if (radioButtonExternalPublic.isChecked) {
-            // External - Public
-            editText.setText(StorageUtils.getTextFromStorage(
-
-            Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOCUMENTS), this, FILENAME, FOLDERNAME))
+            }
 
         } else {
-            // External - Privatex
-            editText.setText(StorageUtils.getTextFromStorage(
-
-            getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!!, this, FILENAME, FOLDERNAME))
+            // 2 - Read from internal storage
+            if (radioButtonInternalVolatile.isChecked) {
+                // Cache
+                editText.setText(
+                    StorageUtils.getTextFromStorage(
+                        cacheDir,
+                        this,
+                        FILENAME,
+                        FOLDERNAME
+                    )
+                )
+            } else {
+                // Normal
+                editText.setText(
+                    StorageUtils.getTextFromStorage(
+                        filesDir,
+                        this,
+                        FILENAME,
+                        FOLDERNAME
+                    )
+                )
+            }
 
         }
 
-        }
+    }
 
+    // 1 - Write internal storage
+    private fun writeInternalStorage() {
+        if (radioButtonInternalVolatile.isChecked) {
+            StorageUtils.setTextInStorage(
+                cacheDir,
+                this,
+                FILENAME,
+                FOLDERNAME,
+                editText.text.toString()
+            )
         } else {
-       // TODO : READ FROM INTERNAL STORAGE
-
-        }
-
-       // 7 - Write external storage
-       private fun writeExternalStorage() {
-
-       if (StorageUtils.isExternalStorageWritable()) {
-       if (radioButtonExternalPublic.isChecked) {
-       StorageUtils.setTextInStorage(
-            Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOCUMENTS),this, FILENAME,FOLDERNAME,this.editText.text.toString())
-
-        } else {
-            StorageUtils.setTextInStorage(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),this,FILENAME,FOLDERNAME,this.editText.text.toString())
-        }
-
-        } else {
-            Toast.makeText(this, getString(R.string.external_storage_impossible_create_file),Toast.LENGTH_LONG).show()
-        }
-
+            StorageUtils.setTextInStorage(
+                filesDir,
+                this,
+                FILENAME,
+                FOLDERNAME,
+                editText.text.toString()
+            )
         }
     }
-}}
+}
+
+// 7 - Write external storage
+private fun writeExternalStorage() {
+
+    if (StorageUtils.isExternalStorageWritable()) {
+        if (radioButtonExternalPublic.isChecked) {
+            StorageUtils.setTextInStorage(
+                Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOCUMENTS
+                ), this, FILENAME, FOLDERNAME, this.editText.text.toString()
+            )
+
+        } else {
+            StorageUtils.setTextInStorage(
+                getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+                this,
+                FILENAME,
+                FOLDERNAME,
+                this.editText.text.toString()
+            )
+        }
+
+    } else {
+        Toast.makeText(
+            this,
+            getString(R.string.external_storage_impossible_create_file),
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+}
+}
